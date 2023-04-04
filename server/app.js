ba0f80ffe559b1ae00f6a92f7686920ed7d791db
zoheballadin <beanzoheb@gmail.com>
2023-04-04 20:06:46 +0530

    bugs

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import userRoutes from "./controllers/user.js"
+import productRoutes from "./controllers/product.js"
+import orderRoutes from "./controllers/order.js"
 import "./dbConnect.js"
+
 const port = 5001;
 
 const app = express();
@@ -9,6 +12,8 @@ app.use(express.json())
 
 
 app.use("/api/user", userRoutes)
+app.use("/api/product", productRoutes)
+app.use("/api/order", orderRoutes)
 
 
 
