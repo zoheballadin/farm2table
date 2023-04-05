7acf63bd6af8e25b497df802c15250881737c6fe
zoheballadin <beanzoheb@gmail.com>
2023-04-05 17:23:46 +0530

    conflict

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Product from "./components/Product2";
 import { UserOrders } from "./components/UserOrders";
 import { OrderInfo } from "./components/OrderInfo";
 import AddProduct from "./components/AddProduct";
+import { SellerOrders } from "./components/SellerOrders";
+import { OrderInfoSeller } from "./components/OrderInfoSeller";
 
 function App() {
   return (
@@ -15,9 +17,11 @@ function App() {
       <Route path="/search" element={<Search/>}/>
       <Route path="/product/:productId" element={<Product/>}/>
       <Route path="/user/orders" element={<UserOrders/>}/>
+      <Route path="/seller/orders" element={<SellerOrders/>}/>
       <Route path="/user/order/:orderId" element={<OrderInfo/>}/>
       <Route path="/product/add" element={<AddProduct/>}/>
 
+      <Route path="/seller/order/:orderId" element={<OrderInfoSeller/>}/>
 
 
     </Routes>
